8c50c902f3dffd7a5988819a3c7dce495af7d262
rojinln <140244488+rojinln@users.noreply.github.com>
2024-05-13 14:42:26 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+class studentInfo {
+    constructor() {
+        this.ID = 0;
+        this.name = '';
+        this.lastName = '';
+        this.birthday = Date;
+        this.major = '';
+        this.uni = '';
+    }
+}
+
+let students = [
+    { name: "Bahar", lastName: "Khouban", ID: 32912023, birthday: "2000-08-19", major: "CS", uni: "Behesthi" },
+    { name: "Rojin", lastName: "LN", ID: 2931, birthday: "1380-08-13", major: "CS", uni: "Behesthi" }
+];
+
+app.get("/", (req, res) => {
+    let tableRows = '';
+    students.forEach(student => {
+        tableRows += `
+            <tr>
+                <td>${student.ID}</td>
+                <td>${student.name}</td>
+                <td>${student.lastName}</td>
+                <td>${student.birthday}</td>
+                <td>${student.major}</td>
+                <td>${student.uni}</td>
+                <td><button>Edit</button><button>Delete</button></td>
+            </tr>
+        `;
+    });
+
+    const htmlContent = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
+    const modifiedHtml = htmlContent.replace('<!-- table rows -->', tableRows);
+    res.send(modifiedHtml);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
