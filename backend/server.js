8d14d43255034a2f463d56be0f4b237d5c2ec0d3
rojinln <140244488+rojinln@users.noreply.github.com>
2024-05-13 18:41:56 +0330

    Update 2 server.js -> can add students to the array

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,12 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 8080;
 
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.resolve(__dirname, 'public')));
-
-app.use( bodyParser.json() );      
-    app.use(bodyParser.urlencoded({    
-        extended: true
-    })
-)
+app.use(express.static(path.resolve(__dirname, 'frontend')));
+console.log(__dirname);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 class studentInfo {
     constructor() {
@@ -33,7 +30,7 @@ let students = [
     { name: "Rojin", lastName: "LN", ID: 2931, birthday: "1380-08-13", major: "CS", uni: "Behesthi" }
 ];
 
-app.get("/", (req, res) => {
+function renderTable(req, res) {
     let tableRows = '';
     students.forEach(student => {
         tableRows += `
@@ -49,9 +46,25 @@ app.get("/", (req, res) => {
         `;
     });
 
-    const htmlContent = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
+    const htmlContent = fs.readFileSync(path.resolve(__dirname, '../frontend', 'index.html'), 'utf8');
     const modifiedHtml = htmlContent.replace('<!-- table rows -->', tableRows);
     res.send(modifiedHtml);
+}
+
+app.get("/", renderTable);
+
+
+app.post("/", (req, res) => {
+    let input = req.body;
+    let newStudent = new studentInfo();
+    newStudent.ID = parseInt(input.ID);
+    newStudent.name = input.name;
+    newStudent.lastName = input.lastName;
+    newStudent.birthday = input.bdayInput;
+    newStudent.major = input.majorInput;
+    newStudent.uni = input.uniInput; 
+    students.push(newStudent);
+    renderTable(req, res);
 });
 
 app.listen(port, () => {
